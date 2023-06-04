da794bfc2e40da5ecff3d718c2662d706c42ac44
NicsonSUPRA <117769670+NicsonSUPRA@users.noreply.github.com>
2023-06-04 14:07:36 -0300

    Create tsv_data.dart

---
+++ tsv_data.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+import 'package:prog2/delimited_data.dart';
+
+class TSVData implements DelimitedData {
+  List<String> fieldsTSV = [];
+  dynamic listofvalues = [];
+
+  @override
+  String get separator => '"\t"';
+
+  @override
+  void load(tsvfile) {
+    if (!tsvfile.contains('.tsv')) throw FormatException("Invalid Format");
+    tsvfile = File(tsvfile).readAsStringSync();
+    data = tsvfile;
+  }
+
+  @override
+  set data(String data) {
+    final values = data.split('\n');
+    for (var value in values) {
+      listofvalues.add(value.split('\t'));
+    }
+    fieldsTSV = listofvalues[0];
+  }
+
+  @override
+  String get data {
+    if (!hasData) return '';
+    String strValues = '';
+    for (int i = 0; i < listofvalues.length; i++) {
+      strValues += (listofvalues[i]
+          .toString()
+          .replaceAll(' ', '\t')
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .replaceAll(',', ''));
+
+      strValues += '\n';
+    }
+    return strValues;
+  }
+
+  @override
+  List<String> get fields {
+    return fieldsTSV;
+  }
+
+  @override
+  void save(String fileName) {
+    String strValues = '';
+    for (int i = 0; i < listofvalues.length; i++) {
+      strValues += (listofvalues[i]
+          .toString()
+          .replaceAll(' ', '\t')
+          .replaceAll('[', '')
+          .replaceAll(']', '')
+          .replaceAll(',', ''));
+      strValues += '\n';
+    }
+
+    final outFile = File(fileName);
+    outFile.createSync(recursive: true);
+    outFile.writeAsStringSync(strValues);
+    print('Status: Saved successfully');
+  }
+
+  @override
+  void clear() {
+    listofvalues = "";
+  }
+
+  @override
+  bool get hasData => listofvalues.isNotEmpty;
+}
